c36de62a9d114da240cb93a2ba699e4f1fc613b7
Daniel <brikbusters@gmail.com>
2020-06-19 21:47:54 -0400

    Fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # Use this script with a clean installation
 mkdir bibles
-wget http://api.heb12.com/translations-json/en/web.json
+wget http://api.heb12.com/translations/json/en/web.json
 node main.js ./web.json web bibles
 cc bibles/web.c main.c -o main
